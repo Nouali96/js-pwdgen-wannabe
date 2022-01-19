9ea9e95aa90f0d78e416c7e2dce08495564835dd
Mohamed Nououali <mohamed.nououali@gmail.com>
2022-01-19 21:14:13 +0100

    esercizio finito

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+let nome= prompt("nome");
+let cognome= prompt("cognome");
+let colore= prompt("colore");
+
+let risultato= `${nome}${cognome}${colore}21`;
+
+document.getElementById("demo").innerHTML = risultato;
+
+
+
+
+
+  
